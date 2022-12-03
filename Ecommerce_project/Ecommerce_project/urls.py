dc34ed1a32c24a7233d9ecfe19dcec0d92f5c2e5
vinshavinod <vinshavinod3@gmail.com>
2022-12-03 10:31:03 +0530

    second commit

--- Ecommerce_project/Ecommerce_project/urls.py
+++ Ecommerce_project/Ecommerce_project/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('customer.urls')),
+    path('reseller/', include('reseller.urls')),
+    path('ecom_admin/', include('ecom_admin.urls')),
    
 ]
--- Ecommerce_project/customer/views.py
+++ Ecommerce_project/customer/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def customer_home(request):
     return render(request,'customer/customer_home.html')
+
+def customer_login(request):
+    return render(request,'customer/login.html')
 
 def customer_myorder(request):
     return render(request,'customer/myorder.html')
 
+def customer_mywishlist(request):
+    return render(request,'customer/my_wishlist.html')
+
+def customer_category(request):
+    return render(request,'customer/category.html')
+
+def modal(request):
+    return render(request,'customer/login_modal.html')
+
+def login1(request):
+    return render(request,'customer/login1.html')
+
+def signup(request):
+    return render(request,'customer/signup.html')
+
+def product_details(request):
+    return render(request,'customer/product_details.html')
+
+def customer_my_account(request):
+    return render(request,'customer/my_account.html')
